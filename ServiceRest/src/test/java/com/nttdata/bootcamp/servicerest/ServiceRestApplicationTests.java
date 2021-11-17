7e2a55a766d891bfd90310dd036b8cd33d258e2c
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-17 10:39:44 +0100

    Add files via upload

---
+++ ServiceRest/src/test/java/com/nttdata/bootcamp/servicerest/ServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.servicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
